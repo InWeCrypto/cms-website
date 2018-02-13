aeddb27c577e83657675dfcb582df90851a9b251
yayanyang <yayanyang@gmail.com>
2018-02-13 22:31:10 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,13 @@ var webpackDevConfig = require("./build/webpack.dev");
 var webpackProConfig = require("./build/webpack.pro");
 var webpackMerge = require("webpack-merge");
 var webpackConfig;
-if (process.env.NODE_ENV === "development") {
-	webpackConfig = webpackMerge(webpackBaseConfig, webpackDevConfig);
-}
-if (process.env.NODE_ENV === "production") {
-	webpackConfig = webpackMerge(webpackBaseConfig, webpackProConfig);
-}
+// if (process.env.NODE_ENV === "development") {
+// 	webpackConfig = webpackMerge(webpackBaseConfig, webpackDevConfig);
+// }
+// if (process.env.NODE_ENV === "production") {
+// 	webpackConfig = webpackMerge(webpackBaseConfig, webpackProConfig);
+// }
+
+webpackConfig = webpackMerge(webpackBaseConfig, webpackProConfig);
 
 module.exports = webpackConfig;
